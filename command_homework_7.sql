98566744e7b2c0240a2d2743d12d4ff4acd24309
demchenkodenis1 <77532132+demchenkodenis1@users.noreply.github.com>
2021-05-20 13:42:07 +0500

    Add files via upload

---
+++ command_homework_7.sql
@@ -0,0 +1,73 @@
+-- Тема “Сложные запросы”
+
+-- 1. Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
+
+select
+	id,
+	name,
+	birthday_at
+from
+	users;
+
+select
+	u.id,
+	u.name,
+	u.birthday_at
+from
+	users as u
+join orders as o on
+	u.id = o.user_id;
+-- 2. Выведите список товаров products и разделов catalogs, который соответствует товару.   
+
+select
+	p.id,
+	p.name,
+	p.price,
+	c.name as catalog
+from
+	products as p
+join catalogs as c on
+	p.catalog_id = c.id;
+
+
+-- 3. (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). Поля from, to и label содержат английские названия городов, поле name — русское. Выведите список рейсов flights с русскими названиями городов.
+
+DROP TABLE IF EXISTS flights;
+CREATE TABLE flights (
+  id SERIAL PRIMARY KEY,
+  `from` VARCHAR(255) COMMENT 'Город отравления',
+  `to` VARCHAR(255) COMMENT 'Город прибытия'
+) COMMENT = 'Рейсы';
+
+INSERT INTO flights (`from`, `to`) VALUES
+('moscow', 'omsk'),
+('novgorod', 'kazan'),
+('irkutsk', 'moscow'),
+('omsk', 'irkutsk'),
+('moscow', 'kazan');
+
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities (
+  id SERIAL PRIMARY KEY,
+  label VARCHAR(255) COMMENT 'Код города',
+  name VARCHAR(255) COMMENT 'Название города'
+) COMMENT = 'Словарь городов';
+
+INSERT INTO cities (label, name) VALUES
+('moscow', 'Москва'),
+('irkutsk', 'Иркутск'),
+('novgorod', 'Новгород'),
+('kazan', 'Казань'),
+('omsk', 'Омск');
+
+SELECT
+  f.id,
+  cities_from.name AS `from`,
+  cities_to.name AS `to`
+FROM flights AS f
+  JOIN cities AS cities_from
+    ON f.from = cities_from.label
+  JOIN cities AS cities_to
+    ON f.to = cities_to.label;
+    
+     
